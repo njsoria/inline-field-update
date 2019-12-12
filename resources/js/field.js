d8dd1461ddfedcd4c17d9f4c309eb0039837b8ba
Nate Soria <nate@devkore.com>
2019-12-12 15:01:09 -0800

    let's try to see if the extended class works BooleanLiveupdate

--- resources/js/field.js
+++ resources/js/field.js
@@ -2,4 +2,7 @@ Nova.booting((Vue, router, store) => {
     Vue.component('index-liveupdate', require('./components/IndexField'))
     Vue.component('detail-liveupdate', require('./components/DetailField'))
     Vue.component('form-liveupdate', require('./components/FormField'))
+    Vue.component('boolean-index-liveupdate', require('./components/BooleanIndexField'))
+    Vue.component('boolean-detail-liveupdate', require('./components/BooleanDetailField'))
+    Vue.component('boolean-form-liveupdate', require('./components/BooleanFormField'))
 })
